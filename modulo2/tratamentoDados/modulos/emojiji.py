8bf01a929cf4507d33caed697d1fc23bdaa430bf
Micael Santos <micaelds243@gmail.com>
2023-03-20 14:36:59 -0300

    correção de conteudo

--- modulo2/tratamentoDados/modulos/emojiji.py
+++ modulo2/tratamentoDados/modulos/emojiji.py
@@ -1,3 +1,10 @@
 import emoji
+# biblioteca que de emojis
 
-print(emoji.emojize("EMOJI:heart:", use_aliases=True))
+print(emoji.emojize('Python é :kissing_heart:', language='alias'))
+# a tag emoji.emojize é importante para gerar emojis
+# outro ponto é que existe emojis que precisam do comando language='alias' para aparecerem
+
+print(emoji.emojize('eu sou de :cancer:', language='alias'))
+
+# https://www.webfx.com/tools/emoji-cheat-sheet/ - Lista emojis para uso
